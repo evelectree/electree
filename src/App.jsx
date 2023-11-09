f2b8920088e2dc691f2d99d9019bff398068af26
anubhavgoel24 <evelectree@gmail.com>
2023-11-09 16:09:36 +0530

    [feat]: faq

--- src/App.jsx
+++ src/App.jsx
@@ -190,7 +190,7 @@ function App() {
         <div className="py-20 bg-gray-50">
           <div className="container mx-auto px-6 md:px-12 xl:px-32 h-screen">
             <div className="mb-32 text-center">
-              <h2 className="mb-4 text-center text-2xl text-gray-900 font-bold md:text-4xl">
+              <h2 className="mb-16 text-3xl font-bold leading-tight sm:text-4xl lg:text-5xl">
                 Meet our Team
               </h2>
               <p className="text-gray-600 lg:w-8/12 lg:mx-auto">
@@ -241,29 +241,119 @@ function App() {
           </div>
         </div>
       </section>
-      <section
-        className="min-h-screen bg-[url('./assets/contact.jpg')] bg-cover bg-center"
-      >
+      <section className="py-10 bg-gray-900 sm:py-16 lg:py-24">
+        <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+          <div className="max-w-2xl mx-auto text-center">
+            <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">
+              Frequently Asked Questions
+            </h2>
+            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-300">
+              Explore the common questions and answers about our product
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 mt-12 md:mt-20 md:grid-cols-2 gap-y-16 gap-x-20">
+            <div className="flex items-start">
+              <div className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                <span className="text-lg font-semibold text-white">?</span>
+              </div>
+              <div className="ml-4">
+                <p className="text-xl font-semibold text-white">
+                  How does EV Charging Network work?
+                </p>
+                <p className="mt-4 text-base text-gray-400">
+                  We connect users to the nearest EV charging stations and offer
+                  exclusive deals through our app!
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start">
+              <div className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                <span className="text-lg font-semibold text-white">?</span>
+              </div>
+              <div className="ml-4">
+                <p className="text-xl font-semibold text-white">
+                  Which financial services do you offer?
+                </p>
+                <p className="mt-4 text-base text-gray-400">
+                  We provide microfinancing options for purchasing EVs and
+                  installing charging stations at homes and businesses.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start">
+              <div className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                <span className="text-lg font-semibold text-white">?</span>
+              </div>
+              <div className="ml-4">
+                <p className="text-xl font-semibold text-white">
+                  Are you compatible with all EVs?
+                </p>
+                <p className="mt-4 text-base text-gray-400">
+                  Yes, our services are compatible with all types of electric
+                  vehicles, including Teslas, Nissan Leafs, and more.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start">
+              <div className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                <span className="text-lg font-semibold text-white">?</span>
+              </div>
+              <div className="ml-4">
+                <p className="text-xl font-semibold text-white">
+                  How do I find EV charging stations?
+                </p>
+                <p className="mt-4 text-base text-gray-400">
+                  Simply download our app and use the interactive map to find
+                  nearby EV charging stations in an instant!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center mt-12 md:mt-20">
+            <div className="px-8 py-4 text-center bg-gray-800 rounded-full">
+              <p className="text-gray-50">
+                Didn’t find the answer you are looking for?{" "}
+                <a
+                  href="#"
+                  title=""
+                  className="text-yellow-300 transition-all duration-200 hover:text-yellow-400 focus:text-yellow-400 hover:underline"
+                >
+                  Contact our support
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="min-h-screen bg-[url('./assets/contact.jpg')] bg-cover bg-center">
         <div className="flex flex-col min-h-screen bg-black/60">
           <div className="container flex flex-col flex-1 px-6 py-12 mx-auto">
             <div className="flex-1 lg:flex lg:items-center lg:-mx-6">
               <div className="text-white lg:w-1/2 lg:mx-6">
                 <h1 className="text-2xl font-semibold capitalize lg:text-3xl">
-                  Get in touch
+                  Partner with us
                 </h1>
 
                 <p className="max-w-xl mt-6">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Voluptatem quo aliquid molestiae hic incidunt beatae placeat
-                  accusantium! Alias ex quisquam ab tempora. Ratione autem
-                  doloremque ducimus numquam doloribus, error sed.
+                  Ready to electrify the world? Let’s embark on this
+                  eco-adventure together and give your business the charge it
+                  needs. Our platform offers a charging station aggregator for
+                  all EVs and a unique microfinance option to help you with your
+                  electric car purchase.
+                  <br></br>
+                  Partner with us today!!
                 </p>
               </div>
 
               <div className="mt-8 lg:w-1/2 lg:mx-6">
                 <div className="w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-gray-900 lg:max-w-xl">
                   <h1 className="text-xl font-medium text-gray-700 dark:text-gray-200">
-                    Contact form
+                    Contact Us
                   </h1>
 
                   <p className="mt-2 text-gray-500 dark:text-gray-400">
@@ -278,6 +368,17 @@ function App() {
                       <input
                         type="text"
                         placeholder="John Doe"
+                        className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                      />
+                    </div>
+
+                    <div className="flex-1 mt-6">
+                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                        Phone
+                      </label>
+                      <input
+                        type="Phone"
+                        placeholder="9191919191"
                         className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
@@ -313,6 +414,7 @@ function App() {
           </div>
         </div>
       </section>
+
       <footer className="bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
