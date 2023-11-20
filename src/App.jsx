9ed87a4f9592df7f707ee67a85831fb1366f6927
anubhavgoel24 <evelectree@gmail.com>
2023-11-20 13:05:49 +0530

    [fix]: adding required fields on contact form

--- src/App.jsx
+++ src/App.jsx
@@ -487,6 +487,7 @@ function App() {
                         placeholder="John Doe"
                         value={formData.fullName}
                         onChange={handleChange}
+                        required
                         className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
@@ -501,6 +502,7 @@ function App() {
                         placeholder="9191919191"
                         value={formData.phone}
                         onChange={handleChange}
+                        required
                         className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
@@ -515,6 +517,7 @@ function App() {
                         placeholder="johndoe@example.com"
                         value={formData.email}
                         onChange={handleChange}
+                        required
                         className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
