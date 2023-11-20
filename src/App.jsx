faed546c870660b7f1c1d9956aff9c2ab52fe361
anubhavgoel24 <evelectree@gmail.com>
2023-11-20 12:33:48 +0530

    [fix]: contact form

--- src/App.jsx
+++ src/App.jsx
@@ -247,37 +247,37 @@ function App() {
         </div>
       </section>
 
-      <section class="pb-32 pt-32 text-center lg:text-left bg-[#a7c957]">
-        <div class="container mx-auto text-center lg:text-left xl:px-32">
-          <div class="grid items-center lg:grid-cols-2">
-            <div class="mb-12 lg:mb-0">
-              <div class="relative z-[1] block rounded-lg bg-[hsla(0,0%,100%,0.55)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.55)] dark:shadow-black/20 md:px-12 lg:-mr-14 backdrop-blur-[30px]">
-                <h2 class="mb-6 text-4xl font-bold">Micro Finance</h2>
-                <p class="mb-12 text-neutral-500 dark:text-neutral-300">
+      <section className="pb-32 pt-32 text-center lg:text-left bg-[#a7c957]">
+        <div className="container mx-auto text-center lg:text-left xl:px-32">
+          <div className="grid items-center lg:grid-cols-2">
+            <div className="mb-12 lg:mb-0">
+              <div className="relative z-[1] block rounded-lg bg-[hsla(0,0%,100%,0.55)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.55)] dark:shadow-black/20 md:px-12 lg:-mr-14 backdrop-blur-[30px]">
+                <h2 className="mb-6 text-4xl font-bold">Micro Finance</h2>
+                <p className="mb-12 text-neutral-500 dark:text-neutral-300">
                   We're dedicated to helping you switch to an EV by providing
                   simple, affordable financing options. With us on your side,
                   you'll be cruising the streets in style and saving the planet
                   at the same time!
                 </p>
 
-                {/* <div class="grid gap-x-6 md:grid-cols-3">
-                  <div class="mb-12 md:mb-0">
-                    <h2 class="text-dark mb-4 text-3xl font-bold">10%</h2>
-                    <h5 class="mb-0 text-lg font-medium text-neutral-500 dark:text-neutral-300">
+                {/* <div className="grid gap-x-6 md:grid-cols-3">
+                  <div className="mb-12 md:mb-0">
+                    <h2 className="text-dark mb-4 text-3xl font-bold">10%</h2>
+                    <h5 className="mb-0 text-lg font-medium text-neutral-500 dark:text-neutral-300">
                       Less sugar
                     </h5>
                   </div>
 
-                  <div class="mb-12 md:mb-0">
-                    <h2 class="text-dark mb-4 text-3xl font-bold">70%</h2>
-                    <h5 class="mb-0 text-lg font-medium text-neutral-500 dark:text-neutral-300">
+                  <div className="mb-12 md:mb-0">
+                    <h2 className="text-dark mb-4 text-3xl font-bold">70%</h2>
+                    <h5 className="mb-0 text-lg font-medium text-neutral-500 dark:text-neutral-300">
                       More flavor
                     </h5>
                   </div>
 
-                  <div class="">
-                    <h2 class="text-dark mb-4 text-3xl font-bold">0%</h2>
-                    <h5 class="mb-0 text-lg font-medium text-neutral-500 dark:text-neutral-300">
+                  <div className="">
+                    <h2 className="text-dark mb-4 text-3xl font-bold">0%</h2>
+                    <h5 className="mb-0 text-lg font-medium text-neutral-500 dark:text-neutral-300">
                       Gluten
                     </h5>
                   </div>
@@ -288,7 +288,7 @@ function App() {
             <div>
               <img
                 src={ProductImage}
-                class="fancy-border-radius rotate-lg-6 w-full shadow-lg dark:shadow-black/20"
+                className="fancy-border-radius rotate-lg-6 w-full shadow-lg dark:shadow-black/20"
                 alt=""
               />
             </div>
